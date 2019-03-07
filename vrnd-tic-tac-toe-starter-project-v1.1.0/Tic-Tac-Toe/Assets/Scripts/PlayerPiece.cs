2e943066ef6f75a5abd21c6e2511ec32603f7b7d
Alanoud Alhamid <alanoudalhamid@users.noreply.github.com>
2019-03-07 06:32:06 +0300

    Initial commit

---
+++ vrnd-tic-tac-toe-starter-project-v1.1.0/Tic-Tac-Toe/Assets/Scripts/PlayerPiece.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPiece : MonoBehaviour {
+    public bool hasBeenPlayed = false;
+    Vector3 OriginalLocation;
+    Vector3 SelectedLocation;
+
+
+
+    // Use this for initialization
+    void Start () {}
+	
+	// Update is called once per frame
+	void Update () {}
+
+    public void inPlay() {
+        OriginalLocation = this.transform.position;
+        SelectedLocation = new Vector3(OriginalLocation.x, OriginalLocation.y + 0.3f, OriginalLocation.z);
+        StartCoroutine(SmoothMove(SelectedLocation));
+        GetComponent<Collider>().enabled = false;
+    }
+
+    public void PlayPiece(GameObject GridPlate) {
+        hasBeenPlayed = true;
+        GridPlate.GetComponent<Collider>().enabled = false;
+        SelectedLocation = new Vector3(GridPlate.transform.position.x, GridPlate.transform.position.y, GridPlate.transform.position.z);
+        this.transform.position = new Vector3(GridPlate.transform.position.x, GridPlate.transform.position.y + 0.3f, GridPlate.transform.position.z);
+        StartCoroutine(SmoothMove(SelectedLocation));
+        //Tell our GameLogic script to occupy the game board array at the right location with a player piece
+    }
+
+    IEnumerator SmoothMove(Vector3 toLocation)
+    {
+        float closeEnough = 0.01f;
+        float distance = (this.transform.position - toLocation).magnitude;
+        WaitForEndOfFrame wait = new WaitForEndOfFrame();
+        while (distance >= closeEnough)
+        {
+            transform.position = Vector3.Slerp(this.transform.position, toLocation, 0.1f);
+            yield return wait;
+            distance = (this.transform.position - toLocation).magnitude;
+        }
+        this.transform.position = toLocation;
+    }
+}
---
+++ vrnd-tic-tac-toe-starter-project-v1.1.0/Tic-Tac-Toe/Assets/Scripts/holdPiece.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class holdPiece : MonoBehaviour {
+    public GameObject GameLogic;
+    public GameObject player;
+    public PlayerPiece pieceBeingHeld;
+    public bool holdingPiece = false;
+
+    void Start () {}
+
+    void Update(){}
+
+        public void grabPiece(PlayerPiece selectedPiece) {
+        if (selectedPiece.GetComponent<PlayerPiece>().hasBeenPlayed == false) {
+            pieceBeingHeld = selectedPiece;
+            holdingPiece = true;
+            selectedPiece.GetComponent<PlayerPiece>().inPlay();
+        }
+    }
+	
+    public void makeMove(GameObject GridPlate)
+    {
+        if (holdingPiece)
+        {
+            holdingPiece = false;
+            pieceBeingHeld.GetComponent<PlayerPiece>().hasBeenPlayed = true;
+            pieceBeingHeld.GetComponent<PlayerPiece>().PlayPiece(GridPlate);
+            GameLogic.GetComponent<GameLogic>().playerMove(GridPlate);
+        }
+    }
+}
+
+
+
+
+
+
+
+
